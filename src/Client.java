7d4df32d558b149fc34a91712fabdd36a1b40905
Keegan Parrotte <kmp3325@rit.edu>
2015-10-07 13:25:55 -0400

    Initial Client written.  Not sure if everything is right, though.

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,34 @@
-public class Client {
+public class Client extends Thread {
+
+    private final Banker banker;
+    private final int nUnits;
+    private final int nRequests;
+    private final long minSleepMillis;
+    private final long maxSleepMillis;
+
+    public Client(String name, Banker banker, int nUnits, int nRequests, long minSleepMillis, long maxSleepMillis) {
+        super(name);
+        this.banker = banker;
+        this.nUnits = nUnits;
+        this.nRequests = nRequests;
+        this.minSleepMillis = minSleepMillis;
+        this.maxSleepMillis = maxSleepMillis;
+    }
+
+    public void run() {
+        banker.setClaim(nUnits);
+        for (int i = 0; i < nRequests; i++) {
+            if (banker.remaining() == 0) {
+                banker.release(nUnits);
+            } else {
+                banker.request(Math.random() * banker.remaining());
+            }
+            try {
+                Thread.sleep((long)(Math.random() * (maxSleepMillis - minSleepMillis)) + minSleepMillis);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        banker.release(banker.allocated());
+    }
 }
